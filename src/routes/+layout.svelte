d58b42ab509272e9c9edd0d0e05a66518db0ab29
John Osterman <ostermanj@gmail.com>
2025-01-20 19:57:17 -0500

    Adds gtag

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,5 +4,18 @@
     import { page } from '$app/state';
     let { children } = $props();
 </script>
+<svelte:head>
+    {#if import.meta.env.PROD }
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-8P9T7XMGMD"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-8P9T7XMGMD');
+        </script>
+    {/if}
+</svelte:head>
 {@render children()}
 <Footer isPoem={page.data.fields?.isPoem as boolean | undefined}></Footer>
