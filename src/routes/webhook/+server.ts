21d7bf0fab420f273f550dce30e695c5b4ff0927
John Osterman <ostermanj@gmail.com>
2025-02-07 17:15:45 -0500

    Finishes webhook api for slashItem (no images)

--- src/routes/webhook/+server.ts
+++ src/routes/webhook/+server.ts
@@ -2,8 +2,11 @@ import type { RequestHandler } from './$types';
 import { B_IDENTIFIER, B_AUTH, C_WEBHOOK_SIGNING_SECRET } from "$env/static/private";
 import { verifyRequest } from "@contentful/node-apps-toolkit";
 import { json, error } from '@sveltejs/kit';
-import { BskyAgent, RichText } from '@atproto/api'
-import { getAssetPromise } from '$lib/contentful';
+import { BskyAgent, RichText } from '@atproto/api';
+
+function resolveLink(type, slug){
+  return `https://osterman.blog/${type}#${slug}`;
+}
 
 export const POST: RequestHandler = async ({request}) => {
     const requestJson = await request.json();
@@ -17,7 +20,7 @@ export const POST: RequestHandler = async ({request}) => {
     if (!isVerified){
         error(403);
     }
-    if ( !requestJson.fields?.blueskyPost /*|| requestJson.sys.revision > 1 */){    
+    if ( !requestJson.fields?.blueskyPost || requestJson.sys.revision > 1 ){    
         return json(requestJson, {
             status: 200,
             statusText: 'No content',
@@ -32,7 +35,7 @@ export const POST: RequestHandler = async ({request}) => {
     });
     // creating richtext
     const rt = new RichText({
-        text: requestJson.fields?.blueskyPost['en-US'],
+        text: `${requestJson.fields?.blueskyPost['en-US']} ${resolveLink(requestJson?.fields.type['en-US'], requestJson?.fields.slug['en-US'])}`
     })
     await rt.detectFacets(agent) // automatically detects mentions and links
     console.log(rt);
@@ -42,27 +45,5 @@ export const POST: RequestHandler = async ({request}) => {
         facets: rt.facets,
         createdAt: new Date().toISOString(),
     };
-    /*
-    "blueskyImages": {
-      "en-US": [
-        {
-          "sys": {
-            "type": "Link",
-            "linkType": "Asset",
-            "id": "6zeKSwitXNqLkhM5VF4wxy"
-          }
-        }
-      ]
-    }
-      */
-    if ( requestJson.fields?.blueskyImages ){
-        const ctflAssetsPromises = requestJson.fields.blueskyImages['en-US'].map(img => getAssetPromise(img.sys.id));
-        const ctflAssets = await Promise.all(ctflAssetsPromises);
-        return json(ctflAssets);
-        //  TODO: GO THROU EACH IMG, CONVERT TO DATAURI, STRIP OUT EXIF, UPLOAD BLOB TO BSKY, GET RESULT, AND ASS TO POST OBJECT
-    } else {
-        // const post = await agent.post(postRecord)
-        // return json(post);
-        return json(requestJson);
-    }
+    return json(postRecord);
 };
