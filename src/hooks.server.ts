595154956d6fc1fcd641ab5d99fbfd55a62a1fc9
John Osterman <ostermanj@gmail.com>
2025-03-02 16:09:26 -0500

    Trying a 200

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,9 +2,8 @@ import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
 	if (event.url.pathname.startsWith('/.netlify')) {
-		return undefined;
+		return new Response(null, { status: 200 });
 	}
-
 	const response = await resolve(event);
 	return response;
 };
