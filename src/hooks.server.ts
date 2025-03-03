62534ac451fb937b002cab01378c04ef359c27db
John Osterman <ostermanj@gmail.com>
2025-03-02 20:50:34 -0500

    Trying toml excludePath again

--- src/hooks.server.ts
+++
@@ -1,9 +0,0 @@
-import type { Handle } from '@sveltejs/kit';
-
-export const handle: Handle = async ({ event, resolve }) => {
-	if (event.url.pathname.startsWith('/.netlify')) {
-		return new Response(null, { status: 200 });
-	}
-	const response = await resolve(event);
-	return response;
-};
